4715972768f72d2eeb2684c426ebc931003dd89c
maxint64 <maxint64@gmail.com>
2013-06-02 04:38:32 +0800

    modify background function

--- js/background.js
+++ js/background.js
@@ -1,9 +1,105 @@
-var debug = false;
+var DEBUG = false;
 
-getTopicInfo = function(html) {
-    var t = $(html);
-    console.log(t);
-}
+getHtml = function(url) {
+    return $.ajax({
+        url: url,
+        async: false,
+        success: function(data) {
+            if (DEBUG) {
+                console.log(data);
+            }
+            return data;
+        },
+        error: function(xhr, err_obj) {
+            if (DEBUG) {
+                console.log(xhr);
+                console.log(error);
+            }
+            return 0;
+        }
+    }).responseText;
+};
+
+calcTime = function(str) {
+    str = str.split(" ");
+    var date = str[0].split("-");
+    var time = str[1].split(":");
+    var i = parseInt;
+    var now = new Date();
+    var pub = new Date();
+    pub.setFullYear(i(date[0]), i(date[1])-1, i(date[2]));
+    pub.setHours(i(time[0]));
+    pub.setMinutes(i(time[1]));
+    pub.setSeconds(i(time[2]));
+    var diff = i((now.getTime() - pub.getTime())/1000, 10);
+
+    //时间差在一小时之内的，显示时间差
+    //在两天内的，显示时间
+    //在一年内的，显示日期
+    //一年以上显示年差
+    if (diff < 60)
+        return diff + "秒前";
+    else if (diff < 3600)
+        return i(diff/60, 10) + "分钟前";
+    else {
+        if (now.getFullYear() == i(date[0]))
+            if (now.getMonth()+1 == i(date[1]))
+                if (now.getDate() == i(date[2]))
+                    return "今天" + time[0] + ":" + time[1];
+                else if (now.getDate() == i(date[2])+1)
+                    return "昨天" + time[0] + ":" + time[1];
+                else
+                    return date[1] + "-" + date[2];
+            else
+               return date[1] + "-" + date[2];
+        else
+            return now.getFullYear() - i(date[0]) + "年前";
+    }
+};
+
+getTopicInfo = function(url) {
+    var html = getHtml(url);
+
+    //GHOST BUG: 之前这里总是报错，在stackoverflow上看到说jQuery不能解析有<head>标签的html,
+    //所以把<body>中的内容取出放在<div>中，但后来又发现就算不这样做也不会报错
+    //html = "<div>" + html.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/g, '') + "</div>";
+
+    if (!! html) {
+        var info = {};
+        var t = $(html);
+        info.group_name = t.find(".group-item .title a").text();
+        info.group_url = t.find(".group-item .title a").attr("href");
+        var page_num = 1;
+        //如果有分页则用最后一页的html替换之前的
+        if (!! t.find(".paginator").size()) {
+            page_num = t.find(".paginator>a:last").text();
+            url += "?start=" + (page_num-1)*100; 
+            html = getHtml(url);
+            if (!! html) {
+                t = $(html);
+            }
+            else {
+                if (DEBUG) {
+                    console.log(url + "NOT FOUND");
+                    console.log(html);
+                }
+                return 0;
+            }
+        }
+        info.cmt_num = (page_num-1)*100 + t.find("ul#comments>li").size();
+        info.last_reply = t.find(".pubtime:last").text();
+        debugger;
+        info.last_reply_ex = calcTime(info.last_reply);
+        return info;
+    }
+    else {
+        if (DEBUG) {
+            console.log(url + " NOT FOUND");
+            console.log(html);
+        }
+        return 0;
+    }
+};
 
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
     if (sender.tab) {
@@ -53,29 +149,7 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
                     del_url = del_topic = top_url = top_topic = [];
                     break;
                 case "query":
-                    $.ajax({
-                        url: msg.topic_url,
-                        success: function(data, str) {
-                            topic_info = getTopicInfo(data);
-                  //          sendResponse({
-                  //              msg: 0,
-                  //              topic_info: topic_info
-                  //          });
-                            if (debug) {
-                                console.log(data);
-                                console.log(str);
-                            }
-                        },
-                        error: function(xhr, err_obj) {
-                            sendResponse({
-                                msg: 1
-                            });
-                            if (debug) {
-                                console.log(xhr);
-                                console.log(err_obj);
-                            }
-                        }
-                    });
+                    sendResponse(getTopicInfo(msg.topic_url));
                     break;
                 default:
                     break;
--- js/myscript.js
+++ js/myscript.js
@@ -44,10 +44,6 @@ $(document).ready(function(){
 		}
 	}
 	
-	var del_ = function(e) {
-		e.remove();
-	}
-	
 	var add_to_del = function() {
         chrome.extension.sendMessage({
             cmd: "append", 
@@ -71,6 +67,36 @@ $(document).ready(function(){
         });
 	}
 
+    var initTop = function() {
+        var tr = "<tr class='pl'><td class='td-subject'></td><td class='td-reply' nowrap='nowrap'></td><td class='td-time'></td><td></td></tr>";
+        var len = local.top_url.length;
+        for (i = 0; i < len; i++ ) {
+            chrome.extension.sendMessage({cmd: "query", topic_url: local.top_url[i]}, function(info) {
+                if (!! info) {
+                    var new_tr = $(tr); 
+                    var a = $("a");
+                    a.attr("href", local.top_url[i]);
+                    a.attr("title", local.top_topic[i]);
+                    a.text(local.top_topic[i]);
+                    new_tr.find(".td-subject").append(a);
+                    new_tr.find(".td-reply").append(info.reply_num + "回应");
+                    new_tr.find(".td-time").attr("title", info.last_reply);
+                    new_tr.find(".td-time").text(info.last_reply_ex);
+                    a = $("a");
+                    a.attr("href", info.group_url);
+                    a.text(info.group_name);
+                    new_tr.find("td:last").append(a);
+                    console.log(new_tr.html());
+                    debugger;
+                    top_(new_tr);
+                }
+                else {
+                    console.log("TOPIC INFO NOT FOUND");
+                }
+            });
+        }
+    };
+
     var local = {};
 
     var init = function() {
@@ -81,20 +107,20 @@ $(document).ready(function(){
 
         chrome.extension.sendMessage({cmd: "all"}, function(data) {
             local = data;
+
             $($("td.td-subject a").get().reverse()).each(function() {
                 href = $(this).attr("href");
-                if (data.del_url.indexOf(href) >= 0) {
-                    del_($(this).parent().parent());
-                    return ;
-                }
-                else if (data.top_url.indexOf(href) >= 0) {
-                    top_($(this).parent().parent());
-                }
+                if (data.del_url.indexOf(href) >= 0 || data.top_url.indexOf(href) >= 0)
+                    $(this).parent().parent().remove();
+            });
+
+            initTop();
+
+            $("td.td-subject a").each(function() {
                 $(this).before("<input name='trashes' type='checkbox' /> ");
             });
         });
 
-        
         $(".dropdown-menu li a").click(function() {
             select($(this).attr("tabindex"));
         });
@@ -103,7 +129,7 @@ $(document).ready(function(){
             $(":checked[name='trashes']").each(function() {
                 local.del_url.push($(this).next("a").attr("href"));
                 local.del_topic.push($(this).next("a").attr("title"));
-                del_($(this).parent().parent());
+                $(this).parent().parent().remove();
             });
             add_to_del();
         });
@@ -120,11 +146,14 @@ $(document).ready(function(){
         
         $("button#cog").click(function() {
             //window.open(chrome.extension.getURL("html/options.html"));
-            //chrome.extension.sendMessage({cmd: "remove"});
-            chrome.extension.sendMessage({
-                cmd: "query",
-                topic_url: "http://www.douban.com/group/topic/39881257/"
-            });
+            chrome.extension.sendMessage({cmd: "remove"});
+            //chrome.extension.sendMessage({
+            //    cmd: "query",
+            //    topic_url: "http://www.douban.com/group/topic/14753859/",
+            //},
+            //function(data) {
+            //    console.log(data);
+            //});
         });
     };
 
