a900d5d4d18f23313ed209efbd4a82358fea78fa
maxint64 <maxint64@gmail.com>
2013-07-01 17:02:42 +0800

    rebuild message.js

--- js/message.js
+++ js/message.js
@@ -6,16 +6,40 @@ function Message(cmd) {
     this.cmd = cmd;
 }
 
-function QueryMessage(cmd, type, urls, simplified) {
-    Message.call(this, cmd);
+function QueryMessage(type, urls, simplified) {
+    Message.call(this, "query");
     this.type = type;
     this.urls = urls;
     this.simplified = simplified;
 }
 
-function ConfigMessage(cmd, property, operation, data) {
-    Message.call(this, cmd);
+function ConfigMessage(property, operation, data) {
+    Message.call(this, "config");
     this.property = property;
     this.operation = operation;
     this.data = data;
 }
+
+var _add_to_favourites = function(data) {
+    chrome.extension.sendMessage(new ConfigMessage("favorites", "append", data));
+};
+
+var _add_to_blacklist = function(data) {
+    chrome.extension.sendMessage(new ConfigMessage("blacklist", "append", data));
+};
+
+var _add_to_keywords = function(data) {
+    chrome.extension.sendMessage(new ConfigMessage("keywords", "append", data));
+};
+
+var _remove_from_favourites = function(data) {
+    chrome.extension.sendMessage(new ConfigMessage("favorites", "remove", data));
+};
+
+var _remove_from_blacklist = function(data) {
+    chrome.extension.sendMessage(new ConfigMessage("blacklist", "remove", data));
+};
+
+var _remove_from_keywords = function(data) {
+    chrome.extension.sendMessage(new ConfigMessage("keywords", "remove", data));
+};
